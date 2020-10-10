1ce87f6789c2db0939406c28b85bb99fd6164907
Marwin Alejo <soymarwin@users.noreply.github.com>
2020-10-10 15:56:03 +0800

    Updated dt_4

--- EE274_ProgEx02/dt_4.m
+++ EE274_ProgEx02/dt_4.m
@@ -5,6 +5,7 @@
 function y = dt_4(x,L)
     y= zeros(1,length(x));
     b = [1];  % input coefficient n
+    a_l = zeros(1,L-1);  %
     a = [1 a_l -0.5 -0.5] % output coefficient n, n-L, n-L-1
     h = impz(b,a); %impulse response
     y = conv(h,x);
