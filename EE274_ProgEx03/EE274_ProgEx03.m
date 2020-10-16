034320800553be52ceb4189fbde19a705e6f3d28
Marwin Alejo <soymarwin@users.noreply.github.com>
2020-10-16 15:49:39 +0800

    codes

--- EE274_ProgEx03/EE274_ProgEx03.m
+++ EE274_ProgEx03/EE274_ProgEx03.m
@@ -15,17 +15,19 @@
 %%
 % $X(z) = \sum_{n=-\infty}^{\infty} (\frac{4}{3})^{1-k} u(k)z^{k-1}$
 %%
-% $X(z) = \sum_{n=0}^{\infty} (\frac{4}{3}) \cdot (\frac{4}{3})^{-k} \cdot z^k \times z^{-1}$
+% $X(z) = \sum_{n=0}^{\infty} (\frac{4}{3}) \cdot ((\frac{4}{3})^{-1})^{k} \cdot ((1/z)^{-1})^{k} \cdot z^{-1}$
 %%
-% $X(z) = (\frac{4z^{-1}}{3}) \ \sum_{n=0}^{\infty} (\frac{3z}{4})^{k}$
+% $X(z) = (\frac{4z^{-1}}{3}) \ \sum_{n=0}^{\infty} (\frac{3}{4z^{-1}})^{k}$
 %%
-% $X(z) = {(\frac{4z^{-1}}{3}) \cdot (\frac{1}{1 - \frac{3z}{4}}), \ 0 \ < \ \mid {z} \mid \ < \ \frac{4}{3}}$
+% $X(z) = {(\frac{4z^{-1}}{3}) \cdot (\frac{1}{1 - \frac{3}{4z^{-1}}}), \ 0 \ < \ \mid {z} \mid \ < \ \frac{4}{3}}$
 %%
-% $or \ X(z) ={\frac{4z^{-1}}{3 - \frac{9z}{4}}, \ 0 \ < \ \mid {z} \mid \ < \ \frac{4}{3}}$
-%%
-% $or \ X(z) ={\frac{16z^{-1}}{12 - 9z}, \ 0 \ < \ \mid {z} \mid \ < \ \frac{4}{3}}$
-%%
-% $or \ X(z) ={\frac{16}{12z - 9z^{2}}, \ 0 \ < \ \mid {z} \mid \ < \ \frac{4}{3}}$
+% $or \ X(z) ={\frac{16z^{-2}}{12z^{-1} - 9}, \ 0 \ < \ \mid {z} \mid \ < \ \frac{4}{3}}$
+
+% z-plane for 1.(a)
+A1_a_a=[-9 12 0];
+A1_a_b=[0 0 16];
+zplane(A1_a_b,A1_a_a);
+
 %% $(b) \ \ x(n) = 2^{- \mid {n} \mid} + (\frac{1}{3})^{\mid {n} \mid}$
 %%
 % $X(z) = \sum_{n=0}^{\infty} 2^{-n}z^{-n} + \sum_{n=0}^{\infty} (\frac{1}{3})^{n}z^{-n}$
@@ -34,6 +36,13 @@
 %%
 % $X(z) = \frac{1}{1-\frac{z^{-1}}{2}}+\frac{1}{1-\frac{z^{-1}}{3}}$
 %%
-% $X(z) = \frac{2z}{2z-1}+\frac{3z}{3z-1}$
+% $X(z) = \frac{2}{2-z^{-1}}+\frac{3}{3-z^{-1}}$
 %%
-% $X(z) = \frac{z(12z-5)}{(2z-1)(3z-1)},\ \frac{1}{3}\ < \ \mid z \mid \ < \ \frac{1}{2}$
+% $X(z) = \frac{12-5z^{-1}}{(2-z^{-1})(3-z^{-1})},\ \frac{1}{3}\ < \ \mid z \mid \ < \ \frac{1}{2}$
+%%
+% $or X(z) = \frac{12-5z^{-1}}{6-5z^{-1}+z^{-2}},\ \frac{1}{3}\ < \ \mid z \mid \ < \ \frac{1}{2}$
+
+% z-plane for 1.(b)
+A1_b_a=[6 -5 1];
+A1_b_b=[12 -5 0];
+zplane(A1_b_b,A1_b_a);
