8f58418446d510de8b35d033fc8419c4e211d39e
Marwin Alejo <soymarwin@users.noreply.github.com>
2020-10-17 15:14:44 +0800

    latest codes

---
+++ EE274_ProgEx03/stepseq0.m
@@ -0,0 +1,6 @@
+% Function to generate unit-step sequence
+% Written by Marwin B. Alejo 2020-20221 for EE274_ProgEx01 19/09/2020
+function [x,n]=stepseq0(n0,n1,n2)
+    n = [n1:n2];
+    x = [(n0-n) <= 0]; %change to less than to satisfy (n)condition.
+end
